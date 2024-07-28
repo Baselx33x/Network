d7df2c5f244c2c82e9f2ed6c55ff5bb2eccef4c7
BaselX <Basel.x33x@gmail.com>
2024-07-28 22:30:00 +0200

    Creating Socket and IP

--- NetworkTraning/NetworkTraning/Program.cs
+++ NetworkTraning/NetworkTraning/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net; 
+using System.Net.Sockets;
+
+
+
+
+// Create a new socket using the InterNetwork address family, Stream socket type, and TCP protocol
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+// Set the IP address to listen on to any available network interface
+IPAddress ipAddress = IPAddress.Any;
+
+// Create an endpoint that consists of the IP address and a specific port number (23000 in this case)
+IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 23000);
+
+// Bind the socket to the local endpoint
+ socket.Bind(localEndPoint);
+
+// Set the socket to listen for incoming connection requests with a backlog of 10
+socket.Listen(10);
+
+// Accept an incoming connection (this will block until a connection is established)
+socket.Accept();
+
+
+
+
+
