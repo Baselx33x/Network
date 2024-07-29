67754d9ccd7fc3c34a45a8896ee63677f7fa8bfc
BaselX <Basel.x33x@gmail.com>
2024-07-29 13:17:31 +0200

    Organization Making Two Functions

--- NetworkTraning/NetworkTraning/Program.cs
+++ NetworkTraning/NetworkTraning/Program.cs
@@ -3,53 +3,62 @@ using System.Net.Sockets;
 using System.Text;
 
 
-
-// Create a new socket using the InterNetwork address family, Stream socket type, and TCP protocol
-Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-// Set the IP address to listen on to any available network interface
-IPAddress ipAddress = IPAddress.Any;
-
-// Create an endpoint that consists of the IP address and a specific port number (23000 in this case)
-IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 23000);
-
-// Bind the socket to the local endpoint
- socket.Bind(localEndPoint);
-
-// Set the socket to listen for incoming connection requests with a backlog of 10
-socket.Listen(10);
-
-
-Console.WriteLine("About To Accept incoming connection !    ");
-
-// Accept an incoming connection (this will block until a connection is established)
-Socket clientSocket = socket.Accept();
-
-Console.WriteLine("Connection Accepted !" + clientSocket.RemoteEndPoint.ToString() + " " + clientSocket.LocalEndPoint.ToString());
-
-
-byte[] buffer = new byte[3];
-int bytesReceived = 0;
-
-while (true)
+Socket ServerSide()
 {
-    bytesReceived = clientSocket.Receive(buffer);
+    // Create a new socket using the InterNetwork address family, Stream socket type, and TCP protocol
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-    Console.WriteLine("Number of Byters Recived : " + bytesReceived);
+    // Set the IP address to listen on to any available network interface
+    IPAddress ipAddress = IPAddress.Any;
 
-    Console.WriteLine("Received: " + Encoding.ASCII.GetString(buffer, 0, bytesReceived));
+    // Create an endpoint that consists of the IP address and a specific port number (23000 in this case)
+    IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 23000);
 
-    if (Encoding.ASCII.GetString(buffer, 0, bytesReceived) == "x")
+    // Bind the socket to the local endpoint
+    socket.Bind(localEndPoint);
+
+    // Set the socket to listen for incoming connection requests with a backlog of 10
+    socket.Listen(10);
+
+    Console.WriteLine("About To Accept incoming connection !    ");
+
+    // Accept an incoming connection (this will block until a connection is established)
+    Socket clientSocket = socket.Accept();
+
+    Console.WriteLine("Connection Accepted !" + " " + clientSocket.LocalEndPoint.ToString());
+
+    return clientSocket;
+}
+
+// Method for handling client-side communication via a socket
+void ClientSide(Socket clientSocket)
+{
+    // Buffer to store received data
+    byte[] buffer = new byte[128];
+    int bytesReceived = 0; // Variable to store the number of bytes received
+
+    // Continuously receive data
+    while (true)
     {
-        break;
-    }
+        // Receive data into the buffer and get the number of bytes received
+        bytesReceived = clientSocket.Receive(buffer);
 
-    clientSocket.Send(buffer, bytesReceived, SocketFlags.None);
+        Console.WriteLine("Number of Bytes Received: " + bytesReceived);
+        Console.WriteLine("Received: " + Encoding.ASCII.GetString(buffer, 0, bytesReceived));
+
+        // Check if the received data is "x" and break if true
+        if (Encoding.ASCII.GetString(buffer, 0, bytesReceived) == "x")
+        {
+            break;
+        }
+
+        // Send the received data back to the client
+        clientSocket.Send(buffer, bytesReceived, SocketFlags.None);
+    }
 }
 
 
-
-
+ClientSide(ServerSide());
 
 
 
